2e13ef89aeaa90822aaecefe3ef03c2c32f8410b
Nam Nguyen <namn@bluemoon.com.vn>
2015-01-31 17:22:26 -0800

    Use UTF8Deserializer to get away with saving to disk
    The previous commit saved the Scala RDD to disk and loaded it up right
    away in Python. This commit uses UTF8Deserializer to directly convert
    the Scala (Java) RDD to Python strings.

--- src/main/python/search_variants_example_brca1.py
+++ src/main/python/search_variants_example_brca1.py
@@ -1,8 +1,9 @@
 # export SPARK_CLASSPATH=...
+import json
 import pyspark
 import pyspark.rdd
 import pyspark.conf
-import json
+from pyspark.serializers import UTF8Deserializer
 
 conf = pyspark.conf.SparkConf()
 sc = pyspark.SparkContext(conf=conf)
@@ -14,14 +15,16 @@ def brca1():
     scala_rdd = (sc._jvm.com.google.cloud.genomics.spark.examples.
         SearchVariantsExampleBRCA1.createJsonRdd(jsc, args))
 
-    INPUT_FILE = 'brca1'
-    scala_rdd.saveAsTextFile(INPUT_FILE)
-    rdd = sc.textFile(INPUT_FILE).map(lambda line: json.loads(line))
+    java_rdd = scala_rdd.toJavaRDD()
 
-    print 'We have %d records that overlap BRCA1.' % rdd.count()
-    print 'But only %d records are of a variant.' % rdd.filter(
+    py_rdd = pyspark.rdd.RDD(java_rdd, sc, UTF8Deserializer())
+    py_rdd = py_rdd.map(lambda line: json.loads(line))
+    py_rdd.cache()
+
+    print 'We have %d records that overlap BRCA1.' % py_rdd.count()
+    print 'But only %d records are of a variant.' % py_rdd.filter(
         lambda (k, v): v['referenceBases'] != 'N').count()
-    print 'The other %d records are reference-matching blocks.' % rdd.filter(
+    print 'The other %d records are reference-matching blocks.' % py_rdd.filter(
         lambda (k, v): v['referenceBases'] == 'N').count()
 
     sc.stop()
